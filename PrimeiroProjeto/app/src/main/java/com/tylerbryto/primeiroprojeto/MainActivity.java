3ccc314d542b754b2bc78a099b87b8d683a94e7d
Tyler Mendes de Brito <tyler.brito99@gmail.com>
2018-07-30 21:46:56 -0300

    primeiro commit

---
+++ PrimeiroProjeto/app/src/main/java/com/tylerbryto/primeiroprojeto/MainActivity.java
@@ -0,0 +1,24 @@
+package com.tylerbryto.primeiroprojeto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sortearNumero(View view) {
+        int numero = new Random().nextInt(11);
+        TextView texto = findViewById(R.id.textoResultado);
+        texto.setText("O número sorteado é: " + numero);
+    }
+}
