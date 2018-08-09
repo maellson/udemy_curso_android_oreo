f27be0fb2936e8c902c35cb178f7f41bb06818ac
tylerbryto <tyler.brito99@gmail.com>
2018-08-08 21:47:26 -0300

    recycler view filmes

---
+++ RecyclerView/app/src/main/java/com/tylerbryto/recyclerview/activity/activity/MainActivity.java
@@ -0,0 +1,80 @@
+package com.tylerbryto.recyclerview.activity.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+import com.tylerbryto.recyclerview.R;
+import com.tylerbryto.recyclerview.activity.RecyclerItemClickListener;
+import com.tylerbryto.recyclerview.activity.adapter.Adapter;
+import com.tylerbryto.recyclerview.activity.model.Filme;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private List<Filme> listaFilmes = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerView);
+
+        criarFilmes();
+
+        Adapter adapter = new Adapter(listaFilmes);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayout.VERTICAL));
+        recyclerView.setAdapter(adapter);
+
+        recyclerView.addOnItemTouchListener(
+                new RecyclerItemClickListener(
+                        getApplicationContext(),
+                        recyclerView,
+                        new RecyclerItemClickListener.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(View view, int position) {
+                                Filme filme = listaFilmes.get( position );
+                                Toast.makeText(
+                                        getApplicationContext(),
+                                        "Item pressionado: " + filme.getTituloFilme(),
+                                        Toast.LENGTH_SHORT
+                                ).show();
+                            }
+
+                            @Override
+                            public void onLongItemClick(View view, int position) {
+                                Filme filme = listaFilmes.get( position );
+                                Toast.makeText(
+                                        getApplicationContext(),
+                                        "Click longo: "  + filme.getTituloFilme(),
+                                        Toast.LENGTH_SHORT
+                                ).show();
+                            }
+
+                            @Override
+                            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                            }
+                        }
+                )
+        );
+    }
+
+    public void criarFilmes() {
+        for (int i = 0; i < 100000; i++) {
+            listaFilmes.add(new Filme("Filme" + (i + 1), "Genero" + (i + 1), "2018"));
+        }
+    }
+}
